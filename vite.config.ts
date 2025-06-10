0238a35c37f062fd7697c7199b3c1e81b057dadf
Santanil Roy <113881586+santanilroy@users.noreply.github.com>
2025-06-10 20:00:53 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/shopshpere",
+  base: "/E-commerce",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
